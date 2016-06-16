1e5585c3e8d41f0e79c042371dc286a672215049
Luis <luisdiazvenero@gmail.com>
2016-06-16 17:42:08 -0500

    ajuste script form php

--- mail/test.php
+++ mail/test.php
@@ -1,11 +1,25 @@
 <?php 
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
-    $from = "noreply@capitanmurdock.com";
-    $to = "luis@coronelsmith.com";
-    $subject = "PHP Mail Test script";
-    $message = "This is a test to check the PHP Mail functionality";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
+
+$from = "noreply@capitanmurdock.com";
+$to = "luis@coronelsmith.com"; 
+$subject = "Lead Clinica Internacional:";
+$message = "Ha recibido un nuevo mensaje desde landing Leads Clinica Internacional. Aquí los detalles:
+   \nNOMBRES: ". $NOMBRES .
+   "\nAPE_PATERNO: ". $APE_PATERNO .
+   "\nAPE_MATERNO: ". $APE_MATERNO .
+   "\nCOD_EMPRESA: ". $COD_EMPRESA .
+   "\nNUM_DOC_IDENTIDAD: ". $NUM_DOC_IDENTIDAD .
+   "\nTIP_DOC_IDENTIDAD: ". $TIP_DOC_IDENTIDAD .
+   "\nFECHA_NACIMIENTO: ". $FECHA_NACIMIENTO .
+   "\nNUM_TELEFONO_1: ". $NUM_TELEFONO_1 .
+   "\nCORREO_1: ". $CORREO_1;
+
+$headers = "From:" . $from;	
+mail($to,$subject,$message,$headers);
+return true;			
+
+
     echo "Test email sent";
 ?>
