6f571364dc80caba9248ae05338b348bc93c7a21
Luis <luisdiazvenero@gmail.com>
2016-06-17 11:51:49 -0500

    ajuste formulario

--- js/registro.js
+++ js/registro.js
@@ -15,17 +15,18 @@ $(function ()
             
             // get values from FORM
             var COD_EMPRESA = "1";
-            var TIP_DOC_IDENTIDAD = $("input[name=tipodocumento]:checked").val();
+            var TIP_DOC_IDENTIDAD = $("select#tipodocumento").val();
             var NUM_DOC_IDENTIDAD = $("input#ndocumento").val();
             var NOMBRES = $("input#nombre").val();
             var APE_PATERNO = $("input#paterno").val();
             var APE_MATERNO = $("input#materno").val();
             var FECHA_NACIMIENTO = $("input#nacimiento").val();
+            var TIP_TELEFONO_1 = $("select#tipotelefono").val();
             var NUM_TELEFONO_1 = $("input#telefono").val();
             var CORREO_1 = $("input#email").val();
             
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "mail/contact_me.php",
                 type: "POST",
                 data: {
                     COD_EMPRESA: COD_EMPRESA,
@@ -35,6 +36,7 @@ $(function ()
                     APE_PATERNO: APE_PATERNO,
                     APE_MATERNO: APE_MATERNO,
                     FECHA_NACIMIENTO: FECHA_NACIMIENTO,
+                    TIP_TELEFONO_1: TIP_TELEFONO_1,
                     NUM_TELEFONO_1: NUM_TELEFONO_1,
                     CORREO_1: CORREO_1
                 },
