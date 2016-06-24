553dd8dcf79c769049e53229d040cfbc2840b54c
Luis <luisdiazvenero@gmail.com>
2016-06-24 16:15:15 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -17,8 +17,10 @@ $data = array("COD_EMPRESA" => 1,
   "ORIGEN" => "EXTRANET",
   "COD_TITULAR" => 0,
   "FLAG_FOTO" => "1");
+
 $data_string = json_encode($data);
 var_dump($data_string);
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario");
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -27,8 +29,9 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'CosapiId: 123'));
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
 $result = curl_exec($ch);
-var_dump($result);
+// var_dump($result);
 
 var_dump(http_response_code());
 ?>
