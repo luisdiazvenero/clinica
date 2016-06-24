cc6b91ac89b438d30a97153bf4d94f4b4161a0d3
Luis <luisdiazvenero@gmail.com>
2016-06-24 16:27:58 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -39,8 +39,8 @@ var_dump(http_response_code());
 
 
 // Parse the response object, e.g. read the headers, body, etc.
-$headers = $response->getHeaders();
-$body = $response->getBody();
+$headers = $result->getHeaders();
+$body = $result->getBody();
 
 // Output headers and body for debugging purposes
 var_dump($headers, $body);
