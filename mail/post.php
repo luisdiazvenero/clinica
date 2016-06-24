4bd1f9654887fd084882d8c4697db0297fb668b7
Luis <luisdiazvenero@gmail.com>
2016-06-24 15:44:04 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -22,6 +22,7 @@ var_dump($data_string);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario");
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'CosapiId: 123'));
