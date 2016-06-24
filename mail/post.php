173f590e6f4e1efe0776ddf0535df653d9ea7673
Luis <luisdiazvenero@gmail.com>
2016-06-24 12:38:30 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -1,16 +1,20 @@
 <?php
-$data = array("name" => "Hagrid", "age" => "36");
-$data_string = json_encode($data);            
-$ch = curl_init('http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario');
+
+
+
+
+$data = array("score" => "234", "playerName" => "Noman", "cheatMode" => "false");
+$data_string = json_encode($data);
+var_dump($data_string);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario");
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ','CosapiId: 123' . strlen($data_string)));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'CosapiId: 123'));
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
-if(!$result)
-curl_error($ch);
-else {
-echo "";
-print_r($result);
-exit;}
+var_dump($result);
+
+
 ?>
