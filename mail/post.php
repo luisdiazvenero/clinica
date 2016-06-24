68117a7b021360d88b67655a0ca7c9107735153a
Luis <luisdiazvenero@gmail.com>
2016-06-24 12:02:18 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -1,22 +1,20 @@
 <?php
 
+$url = 'http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario';
+$data = array('key1' => 'value1', 'key2' => 'value2');
 
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
 
-
-
-
-$data = array("name" => "Hagrid", "age" => "36");                                                                    
-$data_string = json_encode($data). "&";                                                                                   
-                                                                                                                     
-$ch = curl_init('http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario');                                                                      
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json',                                                                                
-    'Content-Length: ' . strlen($data_string))                                                                       
-);                                                                                                                   
-                                                                                                                     
-$result = curl_exec($ch);
+var_dump($result);
 print ($results);
 ?>
