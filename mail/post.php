2c85cb6987eaf93e35970829cc819cb7618fea86
Luis <luisdiazvenero@gmail.com>
2016-06-24 12:25:08 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -1,11 +1,21 @@
 <?php
 $data = array("name" => "Hagrid", "age" => "36");
-$data_string = json_encode($data);            
-$ch = curl_init('http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ','CosapiId: 123' . strlen($data_string)));
-$result = curl_exec($ch);
+$data_string = json_encode($data);
+          
+
+
+
+
+$result = file_get_contents('http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario', null, stream_context_create(array(
+'http' => array(
+'method' => 'POST',
+'header' => 'Content-Type: application/json' . "\r\n"
+. 'CosapiId: 123' . "\r\n"
+. 'Content-Length: ' . strlen($data_string) . "\r\n",
+'content' => $data_string,
+),
+)));
+
 print ($result);
+
 ?>
