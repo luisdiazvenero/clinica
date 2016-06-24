07f00fcd21b9e780b7078fe3a747ddac13d1a78a
Luis <luisdiazvenero@gmail.com>
2016-06-24 12:09:52 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -1,27 +1,46 @@
+<!-- 
+<?php
 
+$url = 'http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario';
+$data = array('key1' => 'value1', 'key2' => 'value2');
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
+print ($results);
+?>
+
+-->
 
 
 <?php
-$url = 'http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario';
-$fields = array(
-            '__VIEWSTATE'=>urlencode($state),
-            '__EVENTVALIDATION'=>urlencode($valid),
-            'btnSubmit'=>urlencode('Submit')
-        );
 
-//url-ify the data for the POST
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-$fields_string = rtrim($fields_string,'&');
+// Script to test if the CURL extension is installed on this server
 
-//open connection
-$ch = curl_init();
+// Define function to test
+function _is_curl_installed() {
+    if  (in_array  ('curl', get_loaded_extensions())) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_POST,count($fields));
-curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
-
-//execute post
-$result = curl_exec($ch);
-print $result;
+// Ouput text to user based on test
+if (_is_curl_installed()) {
+  echo "cURL is <span style=\"color:blue\">installed</span> on this server";
+} else {
+  echo "cURL is NOT <span style=\"color:red\">installed</span> on this server";
+}
 ?>
