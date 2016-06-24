c8990da03b15895677eec7b8f308c5adda4be3bc
Luis <luisdiazvenero@gmail.com>
2016-06-24 11:50:39 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -1,27 +1,39 @@
 <?php
-$url = "http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario";
 
-$data = array(
-  'token' => 'fooToken',
-  'json' => '{"foo":"test"}',
+# An HTTP POST request example
+
+# a pass-thru script to call my Play server-side code.
+# currently needed in my dev environment because Apache and Play run on
+# different ports. (i need to do something like a reverse-proxy from
+# Apache to Play.)
+
+# the POST data we receive from Sencha (which is not JSON)
+$id = $_POST['id'];
+$symbol = $_POST['symbol'];
+$companyName = $_POST['companyName'];
+
+# data needs to be POSTed to the Play url as JSON.
+# (some code from http://www.lornajane.net/posts/2011/posting-json-data-with-php-curl)
+$data = array("id" => "$id", "symbol" => "$symbol", "companyName" => "$companyName");
+$data_string = json_encode($data);
+
+$ch = curl_init('http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))
 );
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 
-foreach($data as $key=>$value) { $content .= $key.'='.$value.'&'; }
+//execute post
+$result = curl_exec($ch);
 
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+//close connection
+curl_close($ch);
 
-$json_response = curl_exec($curl);
+echo $result;
 
-$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-curl_close($curl);
-
-$response = json_decode($json_response, true);
-var_dump($response);
-
-echo json_encode($_POST);
 ?>
