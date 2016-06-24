45e4ec6e8a18ada6b35423c39defa59e88acf3c0
Luis <luisdiazvenero@gmail.com>
2016-06-24 12:48:28 -0500

    Envío PHP

--- mail/post.php
+++ mail/post.php
@@ -3,7 +3,23 @@
 
 
 
-$data = array("score" => "234", "playerName" => "Noman", "cheatMode" => "false");
+$data = array("COD_EMPRESA" => 1,
+  "TIP_DOC_IDENTIDAD" => "0001",
+  "NUM_DOC_IDENTIDAD" => "44020580",
+  "PASSWORD" => "Mn00123456",
+  "COD_USUARIO" => 0,
+  "NOMBRES" => "Brandom",
+  "APE_PATERNO" => "Suarez",
+  "APE_MATERNO" => "Bernaola",
+  "CORREO_1" => "wildatiladsss@gmail.com",
+  "COD_PERFIL" => 0,
+  "FECHA_NACIMIENTO" => "27/10/2015",
+  "COD_SEXO" => "0001",
+  "TIP_TELEFONO_1" => "0002",
+  "NUM_TELEFONO_1" => "963852741",
+  "ORIGEN" => "EXTRANET",
+  "COD_TITULAR" => 0,
+  "FLAG_FOTO" => "1");
 $data_string = json_encode($data);
 var_dump($data_string);
 $ch = curl_init();
@@ -18,3 +34,5 @@ var_dump($result);
 
 
 ?>
+
+
