dad223584892878e96d32059f157de1e2daacc09
Luis <luisdiazvenero@gmail.com>
2016-06-27 11:59:50 -0500

    post request

--- mail/post.php
+++ mail/post.php
@@ -1,42 +1,31 @@
 <?php
 
-$client = new http\Client;
-$request = new http\Client\Request;
+$curl = curl_init();
 
-$body = new http\Message\Body;
-$body->append('{
-  "COD_EMPRESA": 1,
-  "TIP_DOC_IDENTIDAD": "0001",
-  "NUM_DOC_IDENTIDAD": "44020580",
-  "PASSWORD": "Mn00123456",
-  "COD_USUARIO": 0,
-  "NOMBRES": "Brandom",
-  "APE_PATERNO": "Suarez",
-  "APE_MATERNO": "Bernaola",
-  "CORREO_1": "wildatiladsss@gmail.com",
-  "COD_PERFIL": 0,
-  "FECHA_NACIMIENTO": "27/10/2015",
-  "COD_SEXO": "0001",
-  "TIP_TELEFONO_1": "0002",
-  "NUM_TELEFONO_1": "963852741",
-  "ORIGEN": "EXTRANET",
-  "COD_TITULAR": 0,
-  "FLAG_FOTO": "1"
-}
-');
-
-$request->setRequestUrl('http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario');
-$request->setRequestMethod('POST');
-$request->setBody($body);
-
-$request->setHeaders(array(
-  
-  'cache-control' => 'no-cache',
-  'content-type' => 'application/json',
-  'cosapiid' => '123'
+curl_setopt_array($curl, array(
+  CURLOPT_PORT => "81",
+  CURLOPT_URL => "http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\r  \"COD_EMPRESA\": 1,\r  \"TIP_DOC_IDENTIDAD\": \"0001\",\r  \"NUM_DOC_IDENTIDAD\": \"44020580\",\r  \"PASSWORD\": \"Mn00123456\",\r  \"COD_USUARIO\": 0,\r  \"NOMBRES\": \"Brandom\",\r  \"APE_PATERNO\": \"Suarez\",\r  \"APE_MATERNO\": \"Bernaola\",\r  \"CORREO_1\": \"wildatiladsss@gmail.com\",\r  \"COD_PERFIL\": 0,\r  \"FECHA_NACIMIENTO\": \"27/10/2015\",\r  \"COD_SEXO\": \"0001\",\r  \"TIP_TELEFONO_1\": \"0002\",\r  \"NUM_TELEFONO_1\": \"963852741\",\r  \"ORIGEN\": \"EXTRANET\",\r  \"COD_TITULAR\": 0,\r  \"FLAG_FOTO\": \"1\"\r}\r",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "content-type: application/json",
+    "cosapiid: 123"
+  ),
 ));
 
-$client->enqueue($request)->send();
-$response = $client->getResponse();
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-echo $response->getBody();
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
