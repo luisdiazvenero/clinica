cd5ef0d5120f3f7874f955e1ee82ae05af5dac06
Luis <luisdiazvenero@gmail.com>
2016-06-27 12:50:45 -0500

    post request

--- mail/post.php
+++ mail/post.php
@@ -6,8 +6,7 @@ curl_setopt_array($curl, array(
   CURLOPT_PORT => "81",
   CURLOPT_URL => "http://181.65.214.109:81/ServInterno.svc/wsSetRegistrarUsuario",
   CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_SSL_VERIFYPEER => false,
-  CURLOPT_NOSIGNAL => 1,
+  CURLOPT_USERAGENT => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)")
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
@@ -22,6 +21,7 @@ curl_setopt_array($curl, array(
 ));
 
 $response = curl_exec($curl);
+print_r(curl_getinfo($curl));
 $err = curl_error($curl);
 
 curl_close($curl);
